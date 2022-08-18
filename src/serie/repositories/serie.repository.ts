1d37df5d9531ec9a0fcf17d42796e7fa0debf3fe
Romario Marques Melo <romario.melos@gmail.com>
2022-08-18 13:50:58 -0300

    refactor: serie repository

--- src/serie/repositories/serie.repository.ts
+++ src/serie/repositories/serie.repository.ts
@@ -4,31 +4,39 @@ import { Serie } from '../entities/serie.entity';
 
 @EntityRepository(Serie)
 export class SerieRepository extends Repository<Serie> {
+  // Para utilizar a criptografia no PostgreSQL sempre que houver consulta com
+  // dados criptografados é necessário usar o createQueryBuilder to typeorm
+
+  // Para a criptografia feita no PostgreSQL, todo atributo que for criptografada
+  // deve ser envolvida pela função encrypt do PostgreSQL para ser inserida.
   async saveSerie(createSerieDto: CreateSerieDto) {
     return this.createQueryBuilder()
       .insert()
       .into(Serie)
       .values({
-        titulo: () => this.encryptionRaw(createSerieDto.titulo),
-        dataLancamento: () => this.encryptionRaw(createSerieDto.dataLancamento),
-        temporadas: () => this.encryptionRaw(createSerieDto.temporadas),
+        titulo: () =>
+          `encode(encrypt('${createSerieDto.titulo}','${process.env.ENCRYPTION_KEY}','aes'), 'hex')`,
+        dataLancamento: () =>
+          `encode(encrypt('${createSerieDto.dataLancamento}','${process.env.ENCRYPTION_KEY}','aes'), 'hex')`,
+        temporadas: () =>
+          `encode(encrypt('${createSerieDto.temporadas}','${process.env.ENCRYPTION_KEY}','aes'), 'hex')`,
       })
       .execute();
   }
 
+  // Ao buscar a informação no PostgreSQL todas as colunas que forem criptografadas
+  // devem ser envolvida na função decrypt chamando a função addSelect
   async findSerie() {
     return this.createQueryBuilder('serie')
-      .select(this.decryptRaw('titulo'))
-      .addSelect(this.decryptRaw('data_lancamento'))
-      .addSelect(this.decryptRaw('temporadas'))
+      .select(
+        `convert_from(decrypt(concat('\\x', titulo)::bytea, '${process.env.ENCRYPTION_KEY}', 'aes'), 'UTF-8') as titulo`,
+      )
+      .addSelect(
+        `convert_from(decrypt(concat('\\x', data_lancamento)::bytea, '${process.env.ENCRYPTION_KEY}', 'aes'), 'UTF-8') as data_lancamento`,
+      )
+      .addSelect(
+        `convert_from(decrypt(concat('\\x', data_lancamento)::bytea, '${process.env.ENCRYPTION_KEY}', 'aes'), 'UTF-8') as data_lancamento`,
+      )
       .getRawMany();
-  }
-
-  encryptionRaw(data): string {
-    return `encode(encrypt('${data}','${process.env.ENCRYPTION_KEY}','aes'), 'hex')`;
-  }
-
-  decryptRaw(campo: string): string {
-    return `convert_from(decrypt(concat('\\x', ${campo})::bytea, '${process.env.ENCRYPTION_KEY}', 'aes'), 'UTF-8') as ${campo}`;
   }
 }
