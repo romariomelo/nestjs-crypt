d40550a7e126d220b7b4d953fdf6eff7d2a4c8ca
Romario Marques Melo <romario.melos@gmail.com>
2022-08-18 14:32:39 -0300

    doc: comments on dto

--- src/serie/dtos/create-serie.dto.ts
+++ src/serie/dtos/create-serie.dto.ts
@@ -1,16 +1,20 @@
 import { IsDateString, IsNotEmpty, IsNumberString } from 'class-validator';
 
 export class CreateSerieDto {
+  // Como os dados criptografados são caracteres, os tipos dos atributos devem
+  // ser tratados como string, independetemente do formato original. Sendo necessário
+  // converter para o formato correto.
+
   @IsNotEmpty({ message: 'titulo é obrigatório' })
   titulo: string;
 
   @IsNotEmpty({ message: 'dataLancamento é obrigatório' })
   @IsDateString()
-  dataLancamento: string;
+  dataLancamento: string; // Date
 
   @IsNumberString(
     { maxDecimalPlaces: 0 },
     { message: 'temporadas deve ser um numero válido' },
   )
-  temporadas: string;
+  temporadas: string; // number
 }
