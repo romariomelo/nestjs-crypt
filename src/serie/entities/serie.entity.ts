8b29b65378fc155364a36ce00b052b8be6e74a0c
Romario Marques Melo <romario.melos@gmail.com>
2022-08-21 17:28:27 -0300

    doc: comments in entity

--- src/serie/entities/serie.entity.ts
+++ src/serie/entities/serie.entity.ts
@@ -18,24 +18,40 @@ export class Serie {
   @PrimaryGeneratedColumn('increment')
   id: number;
 
+  // Criar nova instância do EncryptionTransformer do TypeORM-Encrypted no transformer
+  // Tamanho do IV precisa ser informado, essa informação é importante para descifrar
+  // IV pode ser gerado aleatoriamente
+  // Algoritmo com tamanho da chave e modo de operação precisam ser informados
   @Column({
     name: 'titulo',
     type: 'varchar',
-    transformer: new EncryptionTransformer(MyEncryptionConfig),
+    transformer: new EncryptionTransformer({
+      ivLength: 16,
+      key: 'e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61',
+      algorithm: 'aes-256-cbc',
+    }),
   })
-  titulo: string;
+  titulo: string; // Atributos precisam ter formato string
 
   @Column({
     name: 'data_lancamento',
     type: 'varchar',
-    transformer: new EncryptionTransformer(MyEncryptionConfig),
+    transformer: new EncryptionTransformer({
+      ivLength: 16,
+      key: 'e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61',
+      algorithm: 'aes-256-cbc',
+    }),
   })
   dataLancamento: string;
 
   @Column({
     name: 'temporadas',
     type: 'varchar',
-    transformer: new EncryptionTransformer(MyEncryptionConfig),
+    transformer: new EncryptionTransformer({
+      ivLength: 16,
+      key: 'e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61',
+      algorithm: 'aes-256-cbc',
+    }),
   })
   temporadas: string;
 
